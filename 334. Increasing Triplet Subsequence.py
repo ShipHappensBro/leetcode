4bae8d4b3927d294054b8dba099262ed7e536e04
ShipHappensBro <100898304+ShipHappensBro@users.noreply.github.com>
2023-08-16 11:49:11 +0300

    16/08/2023

---
+++ 334. Increasing Triplet Subsequence.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first=second=float("inf")
+        for num in nums:
+            if num<=first:
+                first=num
+            elif num<=second:
+                second=num
+            else:
+                return True
+        return False
+run=Solution()
+print(run.increasingTriplet([2,1,5,0,4,6]))
